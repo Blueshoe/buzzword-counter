f4dff8ac56b61f6eea6340e0b53818e651fe38cb
Robert Gutschale <r.gutschale@blueshoe.de>
2020-04-27 11:30:08 +0200

    make buzzword unique

--- buzzword_counter/counter/models.py
+++ buzzword_counter/counter/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Buzzword(models.Model):
-    word = models.CharField(max_length=127)
+    word = models.CharField(max_length=127, unique=True)
     count = models.PositiveSmallIntegerField(blank=True, default=0)
 
     class Meta:
